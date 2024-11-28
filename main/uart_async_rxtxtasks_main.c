14e63705fb4aa227977ba3b05c2f85a7849f7d21
Per Ingemarsson <pi222cp@student.lnu.se>
2024-11-28 11:18:15 +0100

    Add async HTTP get loop

--- main/uart_async_rxtxtasks_main.c
+++ main/uart_async_rxtxtasks_main.c
@@ -1,11 +1,3 @@
-/* UART asynchronous example, that uses separate RX and TX tasks
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
@@ -13,6 +5,15 @@
 #include "driver/uart.h"
 #include "string.h"
 #include "driver/gpio.h"
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include "sdkconfig.h"
+#include "soc/soc_caps.h"
+#include "esp_sleep.h"
+#include "driver/rtc_io.h"
+#include "nvs_flash.h"
+#include "nvs.h"
 
 static const int RX_BUF_SIZE = 1024;
 
@@ -20,7 +21,34 @@ static const int RX_BUF_SIZE = 1024;
 #define TXD_PIN (GPIO_NUM_12)
 #define RXD_PIN (GPIO_NUM_13)
 
-void init(void)
+QueueHandle_t sendControlQueue = NULL;
+QueueHandle_t sleepQueue = NULL;
+QueueHandle_t receiveMsgQueue = NULL;
+QueueHandle_t dataQueue = NULL;
+
+static TimerHandle_t timeoutTimer;
+
+#define queueLength 10
+
+typedef struct
+{
+    const char *command;
+    int waitTimeMs;
+} Command;
+
+typedef enum
+{
+    COMMAND_TYPE_HTTP,
+    COMMAND_TYPE_MQTT
+} CommandType;
+
+typedef enum
+{
+    COMMAND_ACTION_NEXT,
+    COMMAND_ACTION_RESTART
+} CommandAction;
+
+void init_uart(void)
 {
     const uart_config_t uart_config = {
         .baud_rate = 115200,
@@ -36,12 +64,107 @@ void init(void)
     uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 }
 
-int sendData(const char* logName, const char* data)
+int sendData(const char *logName, const char *data)
 {
     const int len = strlen(data);
     const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
-    ESP_LOGI(logName, "Wrote %d bytes", txBytes);
+    ESP_LOGI(logName, "Wrote %d bytes, data: %s", txBytes, data);
+
     return txBytes;
+}
+
+Command *getCommands(CommandType type, size_t *outSize)
+{
+    switch (type)
+    {
+    case COMMAND_TYPE_HTTP:
+    {
+        static Command httpCommands[] = {
+            {"AT+SHREQ=?\r\n", 1000},
+            {"AT+SHREQ?\r\n", 1000},
+            {"AT+CNACT=0,1\r\n", 1000},
+            {"AT+SHCONF=\"URL\",\"http://www.httpbin.org\"\r\n", 1000},
+            {"AT+SHCONF=\"BODYLEN\",1024\r\n", 0},
+            {"AT+SHCONF=\"HEADERLEN\",350\r\n", 0},
+            {"AT+SHCONN\r\n", 1000},
+            {"AT+SHSTATE?\r\n", 1000},
+            {"AT+SHREQ=\"http://www.httpbin.org/get\",1\r\n", 10000},
+            {"AT+SHREAD=0,391\r\n", 3000},
+            {"AT+SHDISC\r\n", 0}};
+        *outSize = sizeof(httpCommands) / sizeof(httpCommands[0]);
+        return httpCommands;
+    }
+    case COMMAND_TYPE_MQTT:
+    {
+        static Command mqttCommands[] = {
+            // TODO: - Add correct MQTT commands
+            // These are just chatgpt generated commands
+            {"AT+CMQTTSTART\r\n", 1000},
+            {"AT+CMQTTACCQ=0,\"clientId123\"\r\n", 1000},
+            {"AT+CMQTTCONNECT=0,\"139.162.164.160\",1883\r\n", 5000},
+            {"AT+CMQTTTOPIC=0,22\r\n", 100},
+            {"example/topic/path\r\n", 100},
+            {"AT+CMQTTPAYLOAD=0,14\r\n", 100},
+            {"{\"key\":\"value\"}\r\n", 100},
+            {"AT+CMQTTPUB=0,1,60\r\n", 5000},
+            {"AT+CMQTTDISC=0,60\r\n", 1000},
+            {"AT+CMQTTSTOP\r\n", 1000}};
+        *outSize = sizeof(mqttCommands) / sizeof(mqttCommands[0]);
+        return mqttCommands;
+    }
+    default:
+    {
+        *outSize = 0;
+        return NULL;
+    }
+    }
+}
+
+static void processCommands(const char *taskTag, Command *commands, size_t commandCount, QueueHandle_t sendControlQueue)
+{
+    size_t i = 0;
+    uint32_t receivedMessage;
+
+    while (1)
+    {
+        // Wait for a message in the sendControlQueue
+        if ((!xQueueReceive(sendControlQueue, &receivedMessage, portMAX_DELAY)) == pdTRUE)
+        {
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        switch (receivedMessage)
+        {
+        case COMMAND_ACTION_NEXT:
+            if (i < commandCount)
+            {
+                sendData(taskTag, commands[i].command);
+                if (commands[i].waitTimeMs > 0)
+                {
+                    vTaskDelay(commands[i].waitTimeMs / portTICK_PERIOD_MS);
+                }
+                i++;
+            }
+            else
+            {
+                ESP_LOGI(taskTag, "Reached end of command list.");
+            }
+            break;
+
+        case COMMAND_ACTION_RESTART:
+            ESP_LOGI(taskTag, "Restarting command list.");
+            i = 0; 
+            // Send the first command again
+            sendData(taskTag, commands[i].command);
+            break;
+
+        default:
+            int msg = receivedMessage;
+            ESP_LOGW(taskTag, "Received unrecognized command: %d", msg);
+            break;
+        }
+    }
 }
 
 static void tx_task(void *arg)
@@ -49,38 +172,89 @@ static void tx_task(void *arg)
     static const char *TX_TASK_TAG = "TX_TASK";
     esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
 
-    while (1) {
-        // A simple http request from 
-        // https://m2msupport.net/m2msupport/http-testing-with-simcom-sim7070-sim7080-modules/
+    size_t commandCount;
+    CommandType currentType = COMMAND_TYPE_HTTP; // Change to COMMAND_TYPE_MQTT for MQTT commands
+    Command *commands = getCommands(currentType, &commandCount);
 
-        sendData(TX_TASK_TAG, "AT+SHREQ=?\r\n");  // Get the support for the commands
-        vTaskDelay(1000 / portTICK_PERIOD_MS);   // WAIT=1
+    // Initial delay to allow system to get ready before sending commands
+    vTaskDelay(20000 / portTICK_PERIOD_MS);
 
-        sendData(TX_TASK_TAG, "AT+SHREQ?\r\n");  // Get the current request
-        vTaskDelay(1000 / portTICK_PERIOD_MS);   // WAIT=1
+    if (commands != NULL)
+    {
+        processCommands(TX_TASK_TAG, commands, commandCount, sendControlQueue);
+    }
 
-        sendData(TX_TASK_TAG, "AT+CNACT=0,1\r\n");  // Activate the network bearer
-        vTaskDelay(1000 / portTICK_PERIOD_MS);   // WAIT=1
+    vQueueDelete(sendControlQueue); // Clean up the queue (though this point is never reached)
+}
 
-        sendData(TX_TASK_TAG, "AT+SHCONF=\"URL\",\"http://www.httpbin.org\"\r\n");  // Set up the HTTP URL
-        vTaskDelay(1000 / portTICK_PERIOD_MS);                                      // WAIT=1
+static void send_next_command()
+{
+    uint32_t action = COMMAND_ACTION_NEXT;
+    ESP_LOGI("Controller", "Sending next command.");
+    xQueueSend(sendControlQueue, &action, portMAX_DELAY);
+}
 
-        sendData(TX_TASK_TAG, "AT+SHCONF=\"BODYLEN\",1024\r\n");  // HTTP body length
-        sendData(TX_TASK_TAG, "AT+SHCONF=\"HEADERLEN\",350\r\n");  // HTTP head length
+static void send_reset_command()
+{
+    uint32_t action = COMMAND_ACTION_RESTART;
+    ESP_LOGI("Controller", "Sending reset command.");
+    xQueueSend(sendControlQueue, &action, portMAX_DELAY);
+}
 
-        sendData(TX_TASK_TAG, "AT+SHCONN\r\n");  // HTTP connection
-        vTaskDelay(1000 / portTICK_PERIOD_MS);   // WAIT=1
+// Timer callback function for if the controller does not receive any data for 15 seconds
+void timeout_callback(TimerHandle_t xTimer)
+{
+    // Log timeout and send reset command
+    uint32_t resetAction = COMMAND_ACTION_RESTART;
+    ESP_LOGI("Controller", "No data received for 15 seconds, sending reset command.");
+    xQueueSend(sendControlQueue, &resetAction, portMAX_DELAY);
 
-        sendData(TX_TASK_TAG, "AT+SHSTATE?\r\n");  // Get the connection status
-        vTaskDelay(1000 / portTICK_PERIOD_MS);   // WAIT=1
+    // Immediately send a next command after the reset
+    send_reset_command();
 
-        sendData(TX_TASK_TAG, "AT+SHREQ=\"http://www.httpbin.org/get\",1\r\n");  // Make the HTTP request
-        vTaskDelay(10000 / portTICK_PERIOD_MS);  // WAIT=10
+    // Reset the timer
+    xTimerReset(timeoutTimer, portMAX_DELAY);
+}
 
-        sendData(TX_TASK_TAG, "AT+SHREAD=0,391\r\n");  // Read the HTTP response
-        vTaskDelay(3000 / portTICK_PERIOD_MS);   // WAIT=3
+static void controller_task(void *arg)
+{
+    timeoutTimer = xTimerCreate("TimeoutTimer", pdMS_TO_TICKS(15000), pdFALSE, NULL, timeout_callback);
+    if (timeoutTimer != NULL)
+    {
+        xTimerStart(timeoutTimer, portMAX_DELAY);
+    }
 
-        sendData(TX_TASK_TAG, "AT+SHDISC\r\n");  // Disconnect HTTP
+    // At task startup, send the first next command
+    send_next_command();
+
+    while (1)
+    {
+        uint8_t *receivedData = NULL; // Pointer to received data
+        if (xQueueReceive(receiveMsgQueue, &receivedData, portMAX_DELAY) == pdTRUE)
+        {
+            if (receivedData != NULL)
+            {
+                ESP_LOGI("Controller", "Received data: %s", receivedData);
+
+                // Reset the timer since data was received
+                xTimerReset(timeoutTimer, portMAX_DELAY);
+
+                // If the data contains a OK in the string, send COMMAND_ACTION_NEXT
+                if (strstr((const char *)receivedData, "OK") != NULL)
+                {
+                    send_next_command();
+                }
+                // If the data contains ERROR in the string, send COMMAND_ACTION_RESTART
+                else if (strstr((const char *)receivedData, "ERROR") != NULL)
+                {
+                    // wait for 0.5 seconds before sending the reset command
+                    vTaskDelay(500 / portTICK_PERIOD_MS);
+                    send_reset_command();
+                }
+
+                free(receivedData);
+            }
+        }
     }
 }
 
@@ -88,21 +262,97 @@ static void rx_task(void *arg)
 {
     static const char *RX_TASK_TAG = "RX_TASK";
     esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
-    uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE + 1);
-    while (1) {
+
+    uint8_t *data = (uint8_t *)malloc(RX_BUF_SIZE + 1);
+    if (data == NULL)
+    {
+        ESP_LOGE(RX_TASK_TAG, "Failed to allocate memory for data buffer");
+        return;
+    }
+
+    while (1)
+    {
         const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
-        if (rxBytes > 0) {
-            data[rxBytes] = 0;
+
+        if (rxBytes == -1)
+        {
+            vTaskDelay(100 / portTICK_PERIOD_MS);
+            ESP_LOGI(RX_TASK_TAG, "Error reading data");
+            continue;
+        }
+
+        if (rxBytes > 0)
+        {
+            data[rxBytes] = 0; // Null-terminate the received data
+
             ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
-            ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+
+            // Allocate a new buffer for each queue item
+            uint8_t *queueData = (uint8_t *)malloc(rxBytes + 1);
+            if (queueData != NULL)
+            {
+                memcpy(queueData, data, rxBytes + 1); // Copy the data and null-terminator
+                if (xQueueSend(receiveMsgQueue, &queueData, portMAX_DELAY) != pdPASS)
+                {
+                    ESP_LOGE(RX_TASK_TAG, "Failed to send data to queue");
+                    free(queueData);
+                }
+            }
+            else
+            {
+                ESP_LOGE(RX_TASK_TAG, "Failed to allocate memory for queue data");
+            }
         }
     }
+
     free(data);
+}
+
+static void deep_sleep_task(void *args)
+{
+    while (1)
+    {
+        uint32_t sleepTime = 10;
+        xQueueSend(sleepQueue, &sleepTime, 0);
+        vTaskDelay(1000000 / portTICK_PERIOD_MS);
+    }
 }
 
 void app_main(void)
 {
-    init();
+    init_uart();
+
+    sendControlQueue = xQueueCreate(queueLength, sizeof(uint32_t));
+    if (sendControlQueue == NULL)
+    {
+        ESP_LOGE("APP_MAIN", "Failed to create sendControlQueue");
+        return;
+    }
+
+    receiveMsgQueue = xQueueCreate(queueLength, sizeof(uint8_t *));
+    if (receiveMsgQueue == NULL)
+    {
+        ESP_LOGE("APP_MAIN", "Failed to create receiveMsgQueue");
+        return;
+    }
+
+    sleepQueue = xQueueCreate(queueLength, sizeof(uint32_t));
+    if (sleepQueue == NULL)
+    {
+        ESP_LOGE("APP_MAIN", "Failed to create sleepQueue");
+        return;
+    }
+
+    dataQueue = xQueueCreate(queueLength, sizeof(uint8_t *));
+    if (dataQueue == NULL)
+    {
+        ESP_LOGE("APP_MAIN", "Failed to create dataQueue");
+        return;
+    }
+
+    // TODO: Set up priority correctly
     xTaskCreate(rx_task, "uart_rx_task", 1024 * 2, NULL, configMAX_PRIORITIES - 1, NULL);
     xTaskCreate(tx_task, "uart_tx_task", 1024 * 2, NULL, configMAX_PRIORITIES - 2, NULL);
+    xTaskCreate(controller_task, "controller_task", 1024 * 2, NULL, configMAX_PRIORITIES - 3, NULL);
+    xTaskCreate(deep_sleep_task, "deep_sleep_task", 1024 * 2, NULL, configMAX_PRIORITIES - 2, NULL);
 }
